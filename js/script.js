6fc459302f02fdb6470e5e9d3a1f48cc72b5b39d
mariana-martins <mariana_martins@id.uff.br>
2017-02-27 10:22:15 +1300

    Fixing problems in NY Times API

--- js/script.js
+++ js/script.js
@@ -23,18 +23,21 @@ function loadData() {
     $body.append('<img class="bgimg" src="' + streetViewUrl + '">');
 
     //API NY Times AJAX request
-    var nytimesUrl = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + cityStr + "&sort=newes&api-key=9a59825425214ed2b45a96e252fd9b14";
+    var nytimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=9a59825425214ed2b45a96e252fd9b14';
+    $.getJSON(nytimesUrl, function(data){
 
-    $.getJSON(nytimesUrl, function (data) {
         $nytHeaderElem.text('New York Times Articles About ' + cityStr);
 
         articles = data.response.docs;
         for (var i = 0; i < articles.length; i++) {
             var article = articles[i];
-            $nytElem.append('<li class="article">' +
-                '<a href="' + article.web_url + '">' + article.headline.main + '</a>' + '<p>' + article.snippet + '</p>' + '</li>')
-        }
-    }).error(function (e) {
+            $nytElem.append('<li class="article">'+
+                '<a href="'+article.web_url+'">'+article.headline.main+'</a>'+
+                '<p>' + article.snippet + '</p>'+
+                '</li>');
+        };
+
+    }).error(function(e){
         $nytHeaderElem.text('New York Times Articles Could Not Be Loaded');
     });
 
