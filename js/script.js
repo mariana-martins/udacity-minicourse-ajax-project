fd1b80fc013b36edd3e9661a29f69bc8e217bb25
mariana-martins <mariana_martins@id.uff.br>
2017-02-27 10:34:57 +1300

    Adding Wikipedia API Ajax Request

--- js/script.js
+++ js/script.js
@@ -22,7 +22,7 @@ function loadData() {
 
     $body.append('<img class="bgimg" src="' + streetViewUrl + '">');
 
-    //API NY Times AJAX request
+    // NY Times API AJAX request
     var nytimesUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=9a59825425214ed2b45a96e252fd9b14';
     $.getJSON(nytimesUrl, function(data){
 
@@ -40,6 +40,30 @@ function loadData() {
     }).error(function(e){
         $nytHeaderElem.text('New York Times Articles Could Not Be Loaded');
     });
+
+    // Wikipedia API AJAX request
+    var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + cityStr + '&format=json&callback=wikiCallback';
+    var wikiRequestTimeout = setTimeout(function(){
+        $wikiElem.text("failed to get wikipedia resources");
+    }, 8000);
+
+    $.ajax({
+        url:wikiUrl,
+        dataType: "jsonp",
+        jsonp: "callback",
+        success: function (response) {
+            var articleList = response[1];
+
+            for (var i = 0; i < articleList.length; i++) {
+                articleStr = articleList[i];
+                var url = 'http://en.wikipedia.org/wiki/' + articleStr;
+                $wikiElem.append('<li><a href="' + url + '">' + articleStr + '</a></li>');
+            };
+
+            clearTimeout(wikiRequestTimeout);
+        }
+    });
+
 
     return false;
 };
